36d3e5cb32a3c8de73822386e59f900b0743f3a4
Ntwali Gislain Muhikira <n.gislain@Ntwalis-MacBook-Air.local>
2024-04-06 21:38:24 -0500

    updating production environment variables, connecting with remote DB

--- SoulsBackendApp/settings.py
+++ SoulsBackendApp/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 import os
 from datetime import timedelta
 from pathlib import Path
 
+import dj_database_url
 from decouple import config
 from dotenv import find_dotenv, load_dotenv
 
@@ -29,11 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # configuration for environment variables
-DB_NAME = os.environ.get("DB_NAME")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_USER = os.environ.get("DB_USER")
-DB_HOST = os.environ.get("DB_HOST ")
-DB_PORT = os.environ.get("DB_PORT")
+DB_URL = os.environ.get("DB_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # duration for auto-generate oauth code validity in minutes
@@ -112,16 +110,7 @@ WSGI_APPLICATION = "SoulsBackendApp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": DB_NAME,
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "HOST": DB_HOST,
-        "PORT": DB_PORT,
-    }
-}
+DATABASES = {"default": dj_database_url.config(default=DB_URL)}
 
 CACHES = {
     "default": {
